066851abaeeed9e86fd260476c9f63c1c464e4c6
Monali Chandurkar <chandurkar.monali123@gmail.com>
2019-06-02 21:02:05 -0700

    updated

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,52 +1,149 @@
-var word = document.getElementById("guessWord");
-console.log(word);
+var guessesLeft = document.getElementById("guessLeft");
+guessesLeft.textContent=9;
+var guessesLeftNum=9;
 
-var singerName = ['Kuldeep','Monali','Sonu','Arijit'];
-var rnum = Math.floor(Math.random() * singerName.length);
-var selectedSinger=singerName[rnum];
-console.log(selectedSinger);
 
-var myFun = function(selectedSinger){
-  
-    for(var i=0;i<selectedSinger.length;i++){
-        word.textContent=word.textContent+""+"-";
+
+
+var wordsPool = ['kuldeep','monali','sonu','arijit'];
+var rnum = Math.floor(Math.random() * wordsPool.length);
+var selectedWord = wordsPool[rnum];
+console.log(selectedWord);
+
+
+var guessWord = document.getElementById("guessWord");
+
+var fillGuessWord = function(){
+
+    for( var i=0; i<selectedWord.length; i++){
+
+        guessWord.textContent = guessWord.textContent+"-";
+
     }
 }
 
-myFun(selectedSinger);
+fillGuessWord();
 
-var guesses = document.getElementById('guessLeft');
-guesses.textContent=9;
-var i=9;
 
-var guessSoFar = document.getElementById("guessSoFar");
-console.log(guessSoFar);
+var guessesSoFar = document.getElementById("guessSoFar");
+
+var wins = document.getElementById("wins");
+wins.textContent=0;
+var w=0;
+
+var losses = document.getElementById("losses");
+losses.textContent=0;
+var l=0;
+
+
+
+
+var fillTheChar = function(letter){
+
+    var count =0;
+
+    var wordsArr = guessWord.textContent.split("");
+
+
+
+    if(guessSoFar.textContent.includes(letter)){
+
+    }
+    else{
+
+        for( var i=0;i<selectedWord.length;i++){
+
+            if(selectedWord[i]===letter){
+    
+                wordsArr[i]=letter;
+    
+               // console.log(selectedWord[i]+" "+letter);
+    
+               // console.log(guessWord.textContent.split(""));
+    
+            }
+            
+    
+        }
+
+        guessSoFar.textContent = guessSoFar.textContent + " "+ event.key;
+
+        guessesLeft.textContent= --guessesLeftNum;
+
+        guessWord.textContent = wordsArr.toString().replace(/,/g,"");
+
+        
+       
+
+        
+
+
+    }
+
+    
+
+   
+
+    
+
+
+}
+
+
+
+var checkForWin = function(word){
+
+    if(guessWord.textContent==selectedWord){
+
+        wins.textContent = ++w; 
+        
+
+    }
+    else{
+
+        losses.textContent = ++l;
+        return;
+
+    }
+
+}
+
+
+
+
+
+
 
 
 document.onkeyup = function(event){
- guessingFun(event.key);
-}
 
-var guessingFun = function(letter){
+    if(guessesLeftNum!=0){
+
+
+        fillTheChar(event.key);
+
+     }
+     else{
+
+
+        if(guessWord.textContent==selectedWord){
+
+            wins.textContent = ++w; 
+            
+            
     
-    if(i!==0){
+        }
+        else if(guessesLeft===0){
+    
+            losses.textContent = ++l;
+          
+            
+    
+        }
+        
+     }
 
-        console.log(letter);
-        replaceChar(letter);
-        guessSoFar.textContent= guessSoFar.textContent + " " +  letter;
-        i--;
-        guesses.textContent=i;
-    }
-
+     
+    
 }
-
- function replaceChar(letter){
-
-    console.log(selectedSinger.indexOf(letter));
-    var ind = selectedSinger.indexOf(letter);
-    console.log(word.textContent.charAt(ind));
-    word.textContent = word.textContent.replace(word.textContent.charAt(ind),letter);
-
-}
-
-
+   
